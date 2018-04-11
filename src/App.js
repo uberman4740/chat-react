d1598653f23921de960e305af14f18ca35fedd35
Karan <karan.sagoo@teradata.com>
2018-04-11 13:29:25 -0700

    init store, reducer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css'
 import uuid from 'uuid'
+import {createStore} from 'redux'
 
+const initialState = {
+    activeThreadId: '1-fca2',
+    threads: [
+        {
+            id: '1-fca2',
+            title: 'Buzz Aldrin',
+            messages: [
+                {
+                    text: 'Twelve minutes to ignition.',
+                    timestamp: Date.now(),
+                    id: uuid.v4(),
+                },
+            ],
+        },
+        {
+            id: '2-be91',
+            title: 'Michael Collins',
+            messages: [],
+        },
+    ],
+};
+
+function reducer(state, action){
+
+}
+const store = createStore(reducer,initialState)
 
 
 class App extends Component {
